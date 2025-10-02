feca792dd91cb78ec8e3463513c29af1af5e23d9
Warriorid <i.hockeyakk@fmail.com>
2025-10-02 23:46:19 +0300

    update model(pit_calculation)

--- internal/app/model/pits_calculations.go
+++ internal/app/model/pits_calculations.go
@@ -6,10 +6,10 @@ type PitsCalculation struct {
     ID          int        `gorm:"primaryKey;column:id;autoIncrement"`
     Status      string     `gorm:"column:status;type:varchar(20);not null;check:status IN ('draft','deleted','formed','completed','rejected')"`
     CreatedAt   time.Time  `gorm:"column:created_at;not null;default:now()"`
-    FormedAt    *time.Time `gorm:"column:formed_at;null"`   
-    CompletedAt *time.Time `gorm:"column:completed_at;null"`
+    FormedAt    time.Time `gorm:"column:formed_at;null"`   
+    CompletedAt time.Time `gorm:"column:completed_at;null"`
     CreatorID   int        `gorm:"column:creator_id;not null"`
-    ModeratorID *int       `gorm:"column:moderator_id;null"`  
+    ModeratorID int       `gorm:"column:moderator_id;null"`  
     
     PitLength   float64 `gorm:"column:pit_length;not null;default:0.1"`
     PitWidth    float64 `gorm:"column:pit_width;not null;default:0.1"`
